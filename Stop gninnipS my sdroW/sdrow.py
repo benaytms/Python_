326115a80ba05697a16ec04c47048539e07206ff
benaytms <benaytomas@gmail.com>
2024-04-23 16:16:45 -0300

    names updated

--- Stop gninnipS my sdroW/sdrow.py
+++
@@ -1,9 +0,0 @@
-def spin_words(sentence):
-    words = sentence.split()
-    for i in range(len(words)):
-        if (len(words[i]) > 4):
-            words[i] = words[i][::-1]
-    sentence = " ".join(words)
-    return sentence
-
-print(spin_words('Macarao com salxixa'))
